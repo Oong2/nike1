df51209bddb1c3ed834c1d75a2451077a29be0a4
kkh <ehddns0528@gmail.com>
2022-03-30 12:49:57 +0900

    220330

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    $(".top-menu-wrap > .top-menu > .top-menu-mid > ul > li").mouseover(function(){
+        $(".top-menu-wrap > .top-menu > .top-menu-mid > ul > li").addClass("active")
+    })
+
+})
