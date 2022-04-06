4639f958fe85754f27ab6951d2a78ab3846fb0ee
kkh <ehddns0528@gmail.com>
2022-04-06 11:27:08 +0900

    220406

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
         $(".top-menu-wrap > .top-menu > .top-menu-mid > ul > li").addClass("active")
     })
 
+
+    $(".slide").slick({
+
+        autoplay : true ,
+        autoplaySpeed : 0 ,
+        speed: 12000,
+        cssEase: "linear" ,
+        arrows: false ,
+        pauseOnHover: false ,
+        slideToShow: 1,
+    })
+
 })
